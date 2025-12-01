22cf41b654d367bcacadf5c990499d5a7dee53d5
adam  boussif <aboussif@ngs-pedago>
2025-12-01 18:52:12 +0000

    install fixed

--- install_explorae.sh
+++ install_explorae.sh
@@ -4,12 +4,12 @@ set -e
 
 # Installation des dépendances Python
 echo "Installation des dépendances du requirements.txt..."
-pip install --break-system-packages -r requirements.txt
+pip install -r requirements.txt
 
 echo "Installation de pyrosetta-installer..."
-pip install --break-system-packages pyrosetta-installer
+pip install pyrosetta-installer
 
 echo "Installation de PyRosetta..."
 python -c 'import pyrosetta_installer; pyrosetta_installer.install_pyrosetta()'
 
-echo "Installation terminée !"
+echo "END"
